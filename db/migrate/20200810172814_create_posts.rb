7790bc201c50e51bd62ade9d48f080b506689cd4
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2020-08-11 18:46:41 +0200

    moved columns creation to create posts migration

--- db/migrate/20200810172814_create_posts.rb
+++ db/migrate/20200810172814_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
+      t.string :postText, null:false
+      t.string :imageURL
+      t.timestamps
     end
   end
 
--- db/migrate/20200810182103_add_post_model_data.rb
+++
@@ -1,6 +0,0 @@
-class AddPostModelData < ActiveRecord::Migration[6.0]
-  def change
-    add_column(:posts, :postText, :string, { null: false })
-    add_column(:posts, :imageURL, :string)
-  end
-end
