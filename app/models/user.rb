5e9fc8682f6142a39ecc6a9b6ef712960116e089
omarsamy09 <omarsamy09@gmail.com>
2020-08-11 17:33:58 +0200

    User model (#3)
    * created user model and schema
    
    * completed validation
    
    * modified some errors

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+      validates :FirstName,presence: true,
+           length:{minimum: 3, maximum: 10 }
+
+      validates :LastName,presence: true,
+           length:{minimum: 3, maximum: 10 }
+
+
+      validates :email,presence: true,
+         format: { with: /\A(.+)@(.+)\z/i, message: "Email invalid"  },
+           uniqueness: { case_sensitive: false },
+           length: {maximum: 254 }
+
+      validates :gender,presence: true
+
+      validates :birthDate,presence:true
+end
