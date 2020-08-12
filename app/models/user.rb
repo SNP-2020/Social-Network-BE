4f2c97ae52c0ec83525ca2770850cb5c3c98122c
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2020-08-12 11:29:12 +0200

    added user-post relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+     has_many :posts
+
       validates :FirstName,presence: true,
            length:{minimum: 3, maximum: 10 }
 
--- db/migrate/20200810172814_create_posts.rb
+++ db/migrate/20200810172814_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.string :postText, null:false
       t.string :imageURL
+      t.belongs_to :user, index: true
       t.timestamps
     end
   end
