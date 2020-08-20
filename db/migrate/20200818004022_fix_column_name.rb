cfe19a0fbfb9460f03ce9ae8468372e2e149f6b2
Omar <omarsamy09@gmail.com>
2020-08-20 03:14:03 +0200

    added authentication and registeration

---
+++ db/migrate/20200818004022_fix_column_name.rb
@@ -0,0 +1,6 @@
+class FixColumnName < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :users, :password, :string
+    add_column :users,:password_digest, :string , null:false
+  end
+end
